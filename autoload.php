0669bd75dee85663c1938be860e271c24009e238
Tadas Motužis <motuzis.tadas@gmail.com>
2017-10-15 17:50:28 +0300

    autoload fixed

--- autoload.php
+++ autoload.php
@@ -8,8 +8,20 @@
 
 spl_autoload_register(function($className)
 {
-    $namespace=str_replace("\\","/",__NAMESPACE__);
-    $className=str_replace("\\","/",$className);
-    $class="app/".(empty($namespace)?"":$namespace."/")."{$className}.php";
-    include_once($class);
+
+    // project-specific namespace prefix if needed
+    $prefix = '';
+
+    $base_dir = __DIR__ . '/app/';
+    $len = strlen($prefix);
+    if (strncmp($prefix, $className, $len) !== 0) {
+        return;
+    }
+
+    $relative_class = substr($className, $len);
+    $file = $base_dir . str_replace('\\', '/', $relative_class) . '.php';
+
+    if (file_exists($file)) {
+        require $file;
+    }
 });
